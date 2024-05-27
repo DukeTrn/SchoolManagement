3e354efac97f7577401b019224922514116d753d
DukeTran <ducdevil2001@gmail.com>
2024-05-27 10:44:50 +0700

    2705: Add models

---
+++ API/SchoolManagement/SchoolManagement.Model/Conduct/ConductDisplayModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolManagement.Model
+{
+    public class ConductDisplayModel
+    {
+        public string ClassName { get; set; } = string.Empty;
+        public string AcademicYear { get; set; } = string.Empty;
+        public int TotalStudents { get; set; }
+        public string HomeroomTeacherName { get; set; } = string.Empty;
+    }
+
+    public class ConductFullDetailModel
+    {
+        public string StudentId { get; set; } = string.Empty;
+        public string StudentName { get; set; } = string.Empty;   
+        public string ConductName { get; set; } = string.Empty;
+        public string Feedback { get; set; } = string.Empty;
+    }
+}
--- API/SchoolManagement/SchoolManagement.Service/ConductService.cs
+++ API/SchoolManagement/SchoolManagement.Service/ConductService.cs
@@ -1,8 +1,22 @@
-﻿using SchoolManagement.Service.Intention;
+﻿using Microsoft.Extensions.Logging;
+using SchoolManagement.Database;
+using SchoolManagement.Entity;
+using SchoolManagement.Service.Intention;
 
 namespace SchoolManagement.Service
 {
     public class ConductService : IConductService
     {
+        private readonly ILogger<ConductService> _logger;
+        private readonly SchoolManagementDbContext _context;
+        
+        public ConductService(ILogger<ConductService> _logger,
+            SchoolManagementDbContext _context)
+        {
+            this._logger = _logger;
+            this._context = _context;
+        }
+
+        
     }
 }
